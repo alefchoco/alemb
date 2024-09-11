cd5723b69b528855d379910a692bfec3734c1e95
alembf <116460755+alembf@users.noreply.github.com>
2024-09-11 11:26:13 -0400

    Update NotesResume.js

--- NotesResume.js
+++ NotesResume.js
@@ -59,6 +59,65 @@ if (time === 'midnight' && dsay ! === 'saturday')
 }
 
 //Used for loops
+  var grocries = 
+    [
+      'apples'
+      'yogurt'
+      'toothpaste'
+  ];
+  for (var element of groceries)
+    {
+      print (element);
+    }
+
+  for (var; = 0; i < 10; i = i + 1)
+  {
+    >1
+    >2
+    >3
+    >4
+  }
 ///Clasic for a loop
+  for (i = 2: i < 7; i+1)
+  {
+    print(i);
+  }
+
+// Nested Loop
+  fro (var outeNumber of
+  [
+      1,
+      2,
+      3,
+  ])
+  fro (var nestedNumber of {
+      4,
+      5,
+      6,
+      ])
+        print(outerNumber, nestedNumber);
+  }]
 //Access a property
+  vvar objectName = {
+    propertyName: 'Coding is fun.' , otherPropertyName;
+  'This is also a key value'
+}
+print (objectName.propertyName);
+
 //A value store in the variable
+var teacup = {
+  red:100,
+  green: 50,
+  Blue:50,
+};
+var sugar = teacup.blue
+
+var myBackpack = {
+  food: [
+    'bananas',
+    'pepsi'
+    ]
+    Equipment: [
+  'map',
+  'something else',
+  ]}; print (myBackpad, food);
