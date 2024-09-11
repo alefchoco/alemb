0e4949fef120c270e5149df25386d9886adb6dd4
alembf <116460755+alembf@users.noreply.github.com>
2024-09-11 10:03:02 -0400

    Create NotesResume.java
    Class notes made in practices for a grasshoper app

---
+++ NotesResume.java
@@ -0,0 +1,64 @@
+var arrayOfColor= [
+  'blue'
+  'orange'
+  ];
+var ramdomCOlor = pickRandom(arrayOfColor)
+
+  //Variables
+
+  var myExmple;
+myExample = 'I created the value';
+print(myExample);
+
+//Arrays
+var exampleArray = [
+  'blue'
+  'orange'
+  5,
+  10,
+  [
+  50,
+  10
+  ]
+
+// if statement
+var aNumber = 5;
+if(aNumber === 5);
+{
+  if (aNumber==4) 
+  {
+    drawBox(red)
+  }
+
+  //User Survey
+  var answer = pickRmdom 
+    ([
+    'head',
+    'tails'.
+    )];
+  Print (answer);
+  If (anwer === 'heads')
+    {
+    drawBox ('gvg ggggwg');
+    }
+
+    //Arothmetic operators
+var wackyArithmeic = 5*5/-5+5;
+var fullName = 'Johnny' + 'Applessed';
+
+var x=5;
+  x=x+3;
+  print ('x is'+x)
+    x=-4;
+  print ('x us now' + x);
+
+//Used a logical operator
+if (time === 'midnight' && dsay ! === 'saturday')
+{
+  print ('you should go to bed')
+}
+
+//Used for loops
+///Clasic for a loop
+//Access a property
+//A value store in the variable
